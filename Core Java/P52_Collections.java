0a6507f88d4f36fe5102c425c8d05b6cbd646ffe
deva <devasharjin@gmail.com>
2026-08-18 17:16:32 +0530

    Collections

---
+++ Core Java/P52_Collections.java
@@ -0,0 +1,22 @@
+// import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class P52_Collections {
+  public static void main(String[] args) {
+    List <Integer> data = new ArrayList<Integer>();  
+    data.add(4);
+    data.add(6);
+    data.add(8);
+    data.add(10);
+
+    System.out.println(data.indexOf(10));
+    
+  // for (int n : data){
+  //   System.out.println(n);
+  // }
+
+
+  }
+  
+}
